55c7b428bc9c5bbfd42269b2734dd33602f0d7eb
张东辉 <1964494455@qq.com>
2025-09-25 14:46:01 +0800

    签名文件

--- zhijin_compass/lib/screens/news/model/news_model.dart
+++ zhijin_compass/lib/screens/news/model/news_model.dart
@@ -20,36 +20,39 @@ class NewsModel {
   });
 
   NewsModel.fromJson(Map<String, dynamic> json) {
-    // 处理url参数，提取url参数中的值
-    // final originalUrl = json['url']?.toString() ?? '';
-    // url = originalUrl; // 默认值
+    //处理url参数，提取url参数中的值
+    final originalUrl = json['url']?.toString() ?? '';
+    url = originalUrl; // 默认值
 
-    // try {
-    //   // 处理哈希(#)后的参数
-    //   final hashIndex = originalUrl.indexOf('#');
-    //   if (hashIndex != -1) {
-    //     final fragment = originalUrl.substring(hashIndex + 1);
-    //     if (fragment.contains('?')) {
-    //       final queryStart = fragment.indexOf('?');
-    //       final queryString = fragment.substring(queryStart + 1);
-    //       final params = Uri.splitQueryString(queryString);
-    //       if (params.containsKey('url')) {
-    //         url = params['url']!;
-    //         debugPrint('成功从片段中提取url: $url');
-    //       }
-    //     }
-    //   }
+    try {
+      // 处理哈希(#)后的参数
+      final hashIndex = originalUrl.indexOf('#');
+      if (hashIndex != -1) {
+        final fragment = originalUrl.substring(hashIndex + 1);
+        if (fragment.contains('?')) {
+          final queryStart = fragment.indexOf('?');
+          final queryString = fragment.substring(queryStart + 1);
+          final params = Uri.splitQueryString(queryString);
+          if (params.containsKey('url')) {
+            url = params['url']!;
+            url =
+                "http://web-test.qcoral.tech/news?flag=2&source=dl&url=${params['url']!}";
+            debugPrint('成功从片段中提取url: $url');
+          }
+        }
+      }
 
-    //   // 检查主查询参数
-    //   final uri = Uri.parse(originalUrl);
-    //   if (uri.queryParameters.containsKey('url')) {
-    //     url = uri.queryParameters['url']!;
-    //     debugPrint('成功从主查询参数中提取url: $url');
-    //   }
-    // } catch (e) {
-    //   debugPrint('解析url失败: $e');
-    // }
-    url = json['url'] + "&source=channel_dl003&link_from=dl";
+      // 检查主查询参数
+      final uri = Uri.parse(originalUrl);
+      if (uri.queryParameters.containsKey('url')) {
+        url =
+            "http://web-test.qcoral.tech/news?flag=2&source=dl&url=${uri.queryParameters['url']!}";
+        debugPrint('成功从主查询参数中提取url: $url');
+      }
+    } catch (e) {
+      debugPrint('解析url失败: $e');
+    }
+
     dlUrl = json['dlUrl'];
     ctime = json['ctime'];
     title = json['title'];
